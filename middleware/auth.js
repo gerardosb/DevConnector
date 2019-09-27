da7372bcb8ef8b5f771cd5673d7fe09a65922108
Gerardo Sanchez <gerardosb95@gmail.com>
2019-09-26 21:34:15 -0300

    Auth middleware now returns the authenticated user instead of only the id

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken')
 const config = require('config')
+const User = require('../models/User')
 
-module.exports = function(req,res,next) {
+module.exports = async function(req,res,next) {
 
     // Get token from header
     const token = req.header('x-auth-token')
@@ -13,8 +14,9 @@ module.exports = function(req,res,next) {
     // verify token
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'))
-
-        req.user = decoded.user
+        // console.log(decoded);
+        const user = await User.findById(decoded.user.id).select('-password')
+        req.user = user
         next()
     } catch (error) {
         res.status(401).json({ msg: 'Token is not valid'})
